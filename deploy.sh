40963de2069002b992f3b2884c61cf3a0864735c
adr <adr@adrhcyahoo.com>
2017-12-28 06:54:37 +0200

    using spring-instrument-tomcat-4.2.9.RELEASE

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 source env.sh
 
 # rm -v ~/apps/opt/tomcat/lib/spring-instrument-tomcat-*.jar 2>/dev/null
-# cp -v /home/adr/.m2/repository/org/springframework/spring-instrument-tomcat/4.0.9.RELEASE/spring-instrument-tomcat-4.0.9.RELEASE.jar ~/apps/opt/tomcat/lib
+# cp -v /home/adr/.m2/repository/org/springframework/spring-instrument-tomcat/4.2.9.RELEASE/spring-instrument-tomcat-4.2.9.RELEASE.jar ~/apps/opt/tomcat/lib
 
 rm -r /home/adr/apps/opt/tomcat/webapps/exifweb
 cp -r target/exifweb /home/adr/apps/opt/tomcat/webapps
