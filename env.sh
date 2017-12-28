734873a66e53a4af39717b6886798cfe59aef1ff
adr <adr@adrhcyahoo.com>
2017-12-28 06:53:37 +0200

    solved wrong jdk in env.sh; changed to:
    export JAVA_HOME=/usr/lib/jvm/java-8-oracle

--- env.sh
+++ env.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-export JAVA_HOME=/home/adr/apps/opt/java/jdk1.7.0_80
-# export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-
+# export JAVA_HOME=/home/adr/apps/opt/java/jdk1.7.0_80
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
