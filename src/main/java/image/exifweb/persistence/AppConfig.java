4be60182d373455cdb829d0247d34d95b8327544
adr <adr@adrhcyahoo.com>
2017-12-28 17:36:37 +0200

    small optimization based on MC - JOverflow

--- src/main/java/image/exifweb/persistence/AppConfig.java
+++ src/main/java/image/exifweb/persistence/AppConfig.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.sql.Timestamp;
-import java.util.Date;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,57 +17,62 @@ import java.util.Date;
 @Entity
 @JsonIgnoreProperties(value = {"hibernateLazyInitializer", "handler"}, ignoreUnknown = true)
 public class AppConfig implements Serializable {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    @Column
-    private String name;
-    @Column
-    private String value;
-    @JsonIgnore
-    @Version
-    @Column(name = "last_update")
-    private Timestamp lastUpdate;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column
+	private String name;
+	@Column
+	private String value;
+	@JsonIgnore
+	@Version
+	@Column(name = "last_update")
+	private Timestamp lastUpdate;
 
-    public Integer getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getValue() {
-        return value;
-    }
+	public String getValue() {
+		return value;
+	}
 
-    public void setValue(String value) {
-        this.value = value;
-    }
+	/**
+	 * small optimization based on MC - JOverflow
+	 *
+	 * @param value
+	 */
+	public void setValue(String value) {
+		this.value = value == null || !value.equals("true") && !value.equals("false") ? value : value.intern();
+	}
 
-    public Timestamp getLastUpdate() {
-        return lastUpdate;
-    }
+	public Timestamp getLastUpdate() {
+		return lastUpdate;
+	}
 
-    public void setLastUpdate(Timestamp lastUpdate) {
-        this.lastUpdate = lastUpdate;
-    }
+	public void setLastUpdate(Timestamp lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
 
-    @Override
-    public String toString() {
-        return "AppConfig{" +
-            "id=" + id +
-            ", name='" + name + '\'' +
-            ", value='" + value + '\'' +
-            ", lastUpdate=" + lastUpdate +
-            '}';
-    }
+	@Override
+	public String toString() {
+		return "AppConfig{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", value='" + value + '\'' +
+				", lastUpdate=" + lastUpdate +
+				'}';
+	}
 }
