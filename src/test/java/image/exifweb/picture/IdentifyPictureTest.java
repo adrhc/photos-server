020e9ba6b7a250ec4ff49c71579ccaa7f419f0b9
adr <adr@adrhcyahoo.com>
2018-02-07 22:27:49 +0200

    formatting

--- src/test/java/image/exifweb/picture/IdentifyPictureTest.java
+++ src/test/java/image/exifweb/picture/IdentifyPictureTest.java
@@ -6,19 +6,19 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class IdentifyPictureTest {
-    public static void main(String[] args) {
-        System.out.println("PATH=" + System.getenv().get("PATH"));
-        try {
-            Process exec = Runtime.getRuntime().exec("identify /home/adr/Pictures/766.jpg");
-            InputStream is = exec.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            String sCurrentLine;
-            while ((sCurrentLine = br.readLine()) != null) {
-                System.out.println(sCurrentLine);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	public static void main(String[] args) {
+		System.out.println("PATH=" + System.getenv().get("PATH"));
+		try {
+			Process exec = Runtime.getRuntime().exec("identify /home/adr/Pictures/766.jpg");
+			InputStream is = exec.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			String sCurrentLine;
+			while ((sCurrentLine = br.readLine()) != null) {
+				System.out.println(sCurrentLine);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 } 
