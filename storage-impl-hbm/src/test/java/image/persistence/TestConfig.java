ef253b4b37353ca0d2e43aa044c203de670c25bb
adr <adr@adrhcyahoo.com>
2018-02-19 22:00:29 +0200

    TestConfig with @Value method parameters

--- storage-impl-hbm/src/test/java/image/persistence/TestConfig.java
+++ storage-impl-hbm/src/test/java/image/persistence/TestConfig.java
@@ -17,17 +17,6 @@ import javax.sql.DataSource;
 @Import(HibernateConfig.class)
 @Profile("test")
 public class TestConfig {
-	@Value("${jdbc.url}")
-	private String jdbcUrl;
-	@Value("${jdbc.userName}")
-	private String userName;
-	@Value("${jdbc.password}")
-	private String password;
-	@Value("${jdbc.minimumIdle}")
-	private int minimumIdle;
-	@Value("${jdbc.maximumPoolSize}")
-	private int maximumPoolSize;
-
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer
 	propertySourcesPlaceholderConfigurer() {
@@ -35,14 +24,18 @@ public class TestConfig {
 	}
 
 	@Bean
-	public DataSource dataSource() {
+	public DataSource dataSource(@Value("${jdbc.url}") String jdbcUrl,
+	                             @Value("${jdbc.userName}") String userName,
+	                             @Value("${jdbc.password}") String password,
+	                             @Value("${jdbc.minimumIdle}") int minimumIdle,
+	                             @Value("${jdbc.maximumPoolSize}") int maximumPoolSize) {
 		HikariDataSource ds = new HikariDataSource();
 		ds.setJdbcUrl(jdbcUrl);
 		ds.setUsername(userName);
 		ds.setPassword(password);
 		ds.setAutoCommit(false);
-		ds.setMinimumIdle(1);
-		ds.setMaximumPoolSize(2);
+		ds.setMinimumIdle(minimumIdle);
+		ds.setMaximumPoolSize(maximumPoolSize);
 		return ds;
 	}
 }
