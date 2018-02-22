9a3e2d0a29ffa724a60672a2c932ad3a26a1331c
adr <adr@adrhcyahoo.com>
2018-02-22 18:34:37 +0200

    env

--- env.sh
+++ env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-# export MVN="mvn"
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export MVN="mvn"
 
-export JAVA_HOME=$TOOLS/jdk1.8.0_141
-export M2_HOME=$TOOLS/maven-3.3.9
-export M2_CONF=$M2_HOME/conf/settings-jisr.xml
-export MVN="$M2_HOME/bin/mvn -s \"$M2_CONF\""
+# export JAVA_HOME=$TOOLS/jdk1.8.0_141
+# export M2_HOME=$TOOLS/maven-3.3.9
+# export M2_CONF=$M2_HOME/conf/settings-jisr.xml
+# export MVN="$M2_HOME/bin/mvn -s \"$M2_CONF\""
