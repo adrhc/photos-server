3b91aa6ccc4a5820cac51ef42c83d5f6ccacce2b
adrian.petre <adrian.petre@uti.ro>
2018-02-23 12:16:08 +0200

    clean install + params

--- clean.install.sh
+++ clean.install.sh
@@ -3,4 +3,4 @@ source env.sh
 
 # $MVN -Dmaven.test.skip=true -Dmaven.javadoc.skip=true clean install
 # $MVN -Dmaven.javadoc.skip=true clean install -P db-on
-$MVN -Dmaven.javadoc.skip=true clean install
+$MVN -Dmaven.javadoc.skip=true clean install "$@"
