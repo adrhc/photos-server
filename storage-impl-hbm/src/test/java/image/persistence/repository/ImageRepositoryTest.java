acb28bc1d2f97bb614b4b5e8f7096ae476dfa353
adr <adr@adrhcyahoo.com>
2018-02-25 14:04:41 +0200

    draft ImageRepositoryTest

---
+++ storage-impl-hbm/src/test/java/image/persistence/repository/ImageRepositoryTest.java
@@ -0,0 +1,51 @@
+package image.persistence.repository;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by adr on 2/25/18.
+ */
+public class ImageRepositoryTest {
+	@Test
+	public void updateThumbLastModifiedForImg() throws Exception {
+
+	}
+
+	@Test
+	public void changeRating() throws Exception {
+
+	}
+
+	@Test
+	public void changeStatus() throws Exception {
+
+	}
+
+	@Test
+	public void getImagesByAlbumId() throws Exception {
+
+	}
+
+	@Test
+	public void persistImage() throws Exception {
+
+	}
+
+	@Test
+	public void markDeleted() throws Exception {
+
+	}
+
+	@Test
+	public void deleteImage() throws Exception {
+
+	}
+
+	@Test
+	public void changeName() throws Exception {
+
+	}
+
+}
