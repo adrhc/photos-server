20c26127a305e7f66dc103028dc103e870dd1878
adr <adr@adrhcyahoo.com>
2018-03-04 17:14:16 +0200

    AppConfigEntityToCdmConverterTest junit5&amp;photos&amp;inmemorydb|misc

--- photos/src/test/java/image/photos/junit5/config/AppConfigEntityToCdmConverterTest.java
+++ photos/src/test/java/image/photos/junit5/config/AppConfigEntityToCdmConverterTest.java
@@ -3,12 +3,14 @@ package image.photos.junit5.config;
 import image.persistence.entity.AppConfig;
 import image.persistence.entity.IAppConfigSupplier;
 import image.photos.config.AppConfigEntityToCdmConverter;
-import org.junit.Test;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Tag("junit5")
+@Tag("photos")
 @Tag("misc")
 public class AppConfigEntityToCdmConverterTest implements IAppConfigSupplier {
 	@Test
