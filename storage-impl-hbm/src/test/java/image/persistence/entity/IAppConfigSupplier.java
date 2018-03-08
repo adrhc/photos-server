54f750dfe93124f57756c0b0d41d54e7d5e014f1
adr <adr@adrhcyahoo.com>
2018-03-08 22:34:44 +0200

    renamed IPositiveRandom to IPositiveIntegerRandom draft IEnhancedRandom

--- storage-impl-hbm/src/test/java/image/persistence/entity/IAppConfigSupplier.java
+++ storage-impl-hbm/src/test/java/image/persistence/entity/IAppConfigSupplier.java
@@ -2,32 +2,10 @@ package image.persistence.entity;
 
 import image.persistence.repository.util.IEnhancedRandom;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 /**
  * Created by adr on 2/26/18.
  */
 public interface IAppConfigSupplier extends IEnhancedRandom {
-	default <T> T randomAppConfig(boolean withId, Class<T> clazz) {
-		return randomAppConfigStream(1, withId, clazz).findAny().get();
-	}
-
-	default <T> List<T> randomAppConfigList(int amount, boolean withId, Class<T> clazz) {
-		return randomAppConfigStream(amount, withId, clazz).collect(Collectors.toList());
-	}
-
-	default <T> Stream<T> randomAppConfigStream(int amount, boolean withId, Class<T> clazz) {
-		if (withId) {
-			return IEnhancedRandom.random.objects(
-					clazz, amount, "lastUpdate");
-		} else {
-			return IEnhancedRandom.random.objects(
-					clazz, amount, "id", "lastUpdate");
-		}
-	}
-
 	default image.cdm.AppConfig fixedCdmAppConfig(boolean withId) {
 		image.cdm.AppConfig appConfig = new image.cdm.AppConfig();
 		appConfig.setId(withId ? 1 : null);
--- storage-impl-hbm/src/test/java/image/persistence/repository/util/IEnhancedRandom.java
+++ storage-impl-hbm/src/test/java/image/persistence/repository/util/IEnhancedRandom.java
@@ -3,8 +3,12 @@ package image.persistence.repository.util;
 import io.github.benas.randombeans.EnhancedRandomBuilder;
 import io.github.benas.randombeans.api.EnhancedRandom;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.nio.charset.Charset.forName;
 
@@ -20,4 +24,24 @@ public interface IEnhancedRandom {
 			.collectionSizeRange(1, 20)
 			.scanClasspathForConcreteTypes(true)
 			.build();
+
+	default <T> T randomAppConfig(boolean withId, Class<T> clazz) {
+		Optional<T> randomT = randomAppConfigStream(1, withId, clazz).findAny();
+		assert randomT.isPresent() : "random " + clazz.getSimpleName() + " is null!";
+		return randomT.get();
+	}
+
+	default <T> List<T> randomAppConfigList(int amount, boolean withId, Class<T> clazz) {
+		return randomAppConfigStream(amount, withId, clazz).collect(Collectors.toList());
+	}
+
+	default <T> Stream<T> randomAppConfigStream(int amount, boolean withId, Class<T> clazz) {
+		if (withId) {
+			return IEnhancedRandom.random.objects(
+					clazz, amount, "lastUpdate");
+		} else {
+			return IEnhancedRandom.random.objects(
+					clazz, amount, "id", "lastUpdate");
+		}
+	}
 }
