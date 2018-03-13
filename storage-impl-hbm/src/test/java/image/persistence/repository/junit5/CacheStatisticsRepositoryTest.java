0ecc96ae55552ac66b51eefdf62916ddbe42a466
adr <adr@adrhcyahoo.com>
2018-03-13 18:43:12 +0200

    CacheStatisticsRepositoryTest

---
+++ storage-impl-hbm/src/test/java/image/persistence/repository/junit5/CacheStatisticsRepositoryTest.java
@@ -0,0 +1,26 @@
+package image.persistence.repository.junit5;
+
+import image.persistence.entity.Album;
+import image.persistence.repository.CacheStatisticsRepository;
+import image.persistence.repository.junit5.testconfig.Junit5HbmInMemoryDbConfig;
+import net.jcip.annotations.NotThreadSafe;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@NotThreadSafe
+@Junit5HbmInMemoryDbConfig
+class CacheStatisticsRepositoryTest {
+	@Inject
+	private CacheStatisticsRepository cacheStatisticsRepository;
+
+	@Test
+	void getSecondLevelCacheStatistics() {
+		Map cacheEntries =
+				this.cacheStatisticsRepository.getSecondLevelCacheStatistics(Album.class.getName());
+		assertNotNull(cacheEntries);
+	}
+}
