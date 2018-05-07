3f90a6f27549dab235feb83e797417b8e1d3f8c9
adr <adr@adrhcyahoo.com>
2018-05-07 21:30:37 +0300

    + comments

--- photos/src/test/java/image/photos/AllTestsSuite.java
+++ photos/src/test/java/image/photos/AllTestsSuite.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 /**
  * https://stackoverflow.com/questions/2176570/how-to-run-all-tests-belonging-to-a-certain-category-in-junit-4/2176791#2176791
  * <p>
+ * used by JsonTestSuite
+ * <p>
  * Created by adrianpetre on 22.02.2018.
  */
 @RunWith(ClasspathSuite.class)
