42b7f8353d4b03da18cff7c3c8b7f50d6ef0968d
adr <adr@adrhcyahoo.com>
2018-05-18 22:35:10 +0300

    these require <forkCount> usage: ./test.sh -P db-off	-> equivalent to in-memory-only ./test.sh -P db-on
    these does not require <forkCount> usage:
    ./test.sh -P staging-only
    ./test.sh -P production-only

--- clean.install.sh
+++ clean.install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 source env.sh
 
+# disable all tests:
 # $MVN -Dmaven.javadoc.skip=true -Dmaven.test.skip=true clean install
-# ./clean.install.sh -P db-off
+
+# these require <forkCount> usage:
+# ./clean.install.sh -P db-off	-> equivalent to in-memory-only
 # ./clean.install.sh -P db-on
+#
+# these does not require <forkCount> usage:
+# ./clean.install.sh -P staging-only
+# ./clean.install.sh -P production-only
 $MVN -Dmaven.javadoc.skip=true clean install "$@"
--- test-report-only.sh
+++ test-report-only.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 source env.sh
 
-# ./test-report-only.sh -P db-off
+# these require <forkCount> usage:
+# ./test-report-only.sh -P db-off	-> equivalent to in-memory-only
 # ./test-report-only.sh -P db-on
-
+#
+# these does not require <forkCount> usage:
+# ./test-report-only.sh -P staging-only
+# ./test-report-only.sh -P production-only
 $MVN surefire-report:report-only
