d772c45acdf72053c171f5c95bccc28eb4e5d9d3
adr <adr@adrhcyahoo.com>
2018-07-08 12:17:30 +0300

    + testing related comments

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 source env.sh
 
+# ehcache static instance is overriden when using stage and production same time so we 
+# need forked tests when running stage and production tests in same mvn test command
+#
 # these require <forkCount> usage:
 # ./test.sh -P db-off	-> equivalent to in-memory-only
 # ./test.sh -P db-on
