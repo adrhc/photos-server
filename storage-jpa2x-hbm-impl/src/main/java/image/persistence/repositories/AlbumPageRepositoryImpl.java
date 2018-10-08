47196a479ed69d905343db0c708a32864ea8de71
adr <adr@adrhcyahoo.com>
2018-10-08 22:16:15 +0300

    AlbumPageRepositoryImplTest

--- storage-jpa2x-hbm-impl/src/main/java/image/persistence/repositories/AlbumPageRepositoryImpl.java
+++ storage-jpa2x-hbm-impl/src/main/java/image/persistence/repositories/AlbumPageRepositoryImpl.java
@@ -51,6 +51,46 @@ public class AlbumPageRepositoryImpl implements AlbumPageRepository {
 	@Override
 	public List<AlbumPage> getPageFromDb(int pageNr, ESortType sort, String toSearch,
 	                                     boolean viewHidden, boolean viewOnlyPrintable, Integer albumId) {
-		return null;
+		boolean emptyAlbumId = albumId == null || albumId.equals(NULL_ALBUM_ID);
+		TypedQuery<AlbumPage> q;
+		if (StringUtils.hasText(toSearch)) {
+			q = this.em.createQuery("SELECT new image.cdm.album.page.AlbumPage(" +
+					"i.id, i.name, i.flags.hidden, i.flags.personal, i.flags.ugly, i.flags.duplicate, " +
+					"i.flags.printable, i.imageMetadata.exifData.imageHeight, " +
+					"i.imageMetadata.exifData.imageWidth, i.rating, a.cover.id, " +
+					"i.imageMetadata.thumbLastModified, i.imageMetadata.dateTime, " +
+					"i.lastUpdate, a.name, a.lastUpdate) " +
+//					"thumbPath(a.name, i.imageMetadata.thumbLastModified, i.name), " +
+//					"imagePath(a.name, i.imageMetadata.thumbLastModified, i.name)) " +
+					"FROM Image i JOIN i.album a " +
+					(emptyAlbumId ? "WHERE i.deleted = false " : "JOIN i.album a WHERE a.id = :albumId AND i.deleted = false ") +
+					VIEW_HIDDEN_SQL + VIEW_PRINTABLE_SQL +
+					"AND i.name LIKE :toSearch " +
+					"ORDER BY i.imageMetadata.exifData.dateTimeOriginal " + sort, AlbumPage.class);
+			// searches case-sensitive for name!
+			q.setParameter("toSearch", "%" + toSearch + "%");
+		} else {
+			q = this.em.createQuery("SELECT new image.cdm.album.page.AlbumPage(" +
+					"i.id, i.name, i.flags.hidden, i.flags.personal, i.flags.ugly, i.flags.duplicate, " +
+					"i.flags.printable, i.imageMetadata.exifData.imageHeight, " +
+					"i.imageMetadata.exifData.imageWidth, i.rating, a.cover.id, " +
+					"i.imageMetadata.thumbLastModified, i.imageMetadata.dateTime, " +
+					"i.lastUpdate, a.name, a.lastUpdate) " +
+//					"thumbPath(a.name, i.imageMetadata.thumbLastModified, i.name), " +
+//					"imagePath(a.name, i.imageMetadata.thumbLastModified, i.name)) " +
+					"FROM Image i JOIN i.album a " +
+					(emptyAlbumId ? "WHERE i.deleted = false " : "JOIN i.album a WHERE a.id = :albumId AND i.deleted = false ") +
+					VIEW_HIDDEN_SQL + VIEW_PRINTABLE_SQL +
+					"ORDER BY i.imageMetadata.exifData.dateTimeOriginal " + sort, AlbumPage.class);
+			q.setHint(QueryHints.HINT_CACHEABLE, !viewHidden && !viewOnlyPrintable);
+		}
+		if (!emptyAlbumId) {
+			q.setParameter("albumId", albumId);
+		}
+		q.setParameter("viewHidden", viewHidden);
+		q.setParameter("viewOnlyPrintable", viewOnlyPrintable);
+		q.setFirstResult((pageNr - 1) * this.appConfigRepository.getPhotosPerPage());
+		q.setMaxResults(this.appConfigRepository.getPhotosPerPage());
+		return q.getResultList();
 	}
 }
--- storage-jpa2x-hbm-impl/src/test/java/image/persistence/repositories/AlbumPageRepositoryImplTest.java
+++ storage-jpa2x-hbm-impl/src/test/java/image/persistence/repositories/AlbumPageRepositoryImplTest.java
@@ -1,5 +1,7 @@
 package image.persistence.repositories;
 
+import exifweb.util.MiscUtils;
+import image.cdm.album.page.AlbumPage;
 import image.cdm.image.status.EImageStatus;
 import image.persistence.config.Junit5Jpa2xInMemoryDbConfig;
 import image.persistence.entity.Album;
@@ -8,11 +10,13 @@ import image.persistence.entity.Image;
 import image.persistence.entity.enums.AppConfigEnum;
 import image.persistence.entity.image.IImageFlagsUtils;
 import image.persistence.repository.AlbumPageRepository;
+import image.persistence.repository.ESortType;
 import image.persistence.repository.util.random.RandomBeansExtensionEx;
 import io.github.glytching.junit.extension.random.Random;
 import lombok.extern.slf4j.Slf4j;
 import net.jcip.annotations.NotThreadSafe;
 import org.junit.Assert;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -23,12 +27,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static image.persistence.repository.AlbumPageRepository.NULL_ALBUM_ID;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @ExtendWith(RandomBeansExtensionEx.class)
 @NotThreadSafe
 @Junit5Jpa2xInMemoryDbConfig
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Slf4j
-class AlbumPageRepositoryImplTest implements IAppConfigSupplier, IImageFlagsUtils {
+class AlbumPageRepositoryImplTest implements IAppConfigSupplier, IImageFlagsUtils, MiscUtils {
 	private static final String T1_TO_SEARCH = "DSC_1555";
 	private static final int PAGE_SIZE = 20;
 	@Autowired
@@ -70,6 +80,12 @@ class AlbumPageRepositoryImplTest implements IAppConfigSupplier, IImageFlagsUtil
 				entityAppConfigOf(AppConfigEnum.photos_per_page, String.valueOf(PAGE_SIZE)));
 	}
 
+	@AfterAll
+	void afterAll() {
+		ignoreExc(() -> this.albumRepository.deleteById(this.albumId));
+		ignoreExc(() -> this.appConfigRepository.deleteByEnumeratedName(AppConfigEnum.photos_per_page));
+	}
+
 	@Test
 	void counting1Page() {
 		int pageCount = this.albumPageRepository.countPages(
@@ -99,5 +115,74 @@ class AlbumPageRepositoryImplTest implements IAppConfigSupplier, IImageFlagsUtil
 						"viewOnlyPrintable = false, albumId = {}",
 				pageCount, "", this.albumId);
 		Assert.assertEquals(3, pageCount);
+	}
+
+	@Test
+	void finding1Image() {
+		List<AlbumPage> imagesForPage = this.albumPageRepository.getPageFromDb(1,
+				ESortType.ASC, T1_TO_SEARCH, true, false, this.albumId);
+		log.debug("imagesForPage.size = {}, sort ASC, searching \"{}\", hidden = true, " +
+						"viewOnlyPrintable = false, albumId = {}", imagesForPage.size(),
+				T1_TO_SEARCH, this.albumId);
+		assertThat(imagesForPage, hasSize(1));
+	}
+
+	@Test
+	void finding1FullPageForAlbum() {
+		List<AlbumPage> imagesForPage = this.albumPageRepository.getPageFromDb(1,
+				ESortType.ASC, "", true, false, this.albumId);
+		log.debug("imagesForPage.size = {}, sort ASC, searching \"{}\", hidden = true, " +
+						"viewOnlyPrintable = false, albumId = {}",
+				imagesForPage.size(), "", this.albumId);
+		assertThat(imagesForPage, hasSize(PAGE_SIZE));
+	}
+
+	@Test
+	void finding1FullPageInAllAlbums() {
+		List<AlbumPage> imagesForPage1 = this.albumPageRepository.getPageFromDb(1,
+				ESortType.ASC, "", true, false, null);
+		log.debug("imagesForPage.size = {}, sort ASC, searching \"\", hidden = true, " +
+				"viewOnlyPrintable = false, albumId = null", imagesForPage1.size());
+		assertThat(imagesForPage1, hasSize(PAGE_SIZE));
+		List<AlbumPage> imagesForPage2 = this.albumPageRepository.getPageFromDb(1,
+				ESortType.ASC, "", true, false, NULL_ALBUM_ID);
+		log.debug("imagesForPage.size = {}, sort ASC, searching \"\", hidden = true, " +
+				"viewOnlyPrintable = false, albumId = {}", imagesForPage2.size(), NULL_ALBUM_ID);
+		assertThat(imagesForPage2, hasSize(PAGE_SIZE));
+	}
+
+	@Test
+	void findPrintableInAllAlbums() {
+		List<AlbumPage> imagesForPage1 = this.albumPageRepository.getPageFromDb(1,
+				ESortType.ASC, "", false, true, null);
+		log.debug("imagesForPage.size = {}, sort ASC, searching \"\", hidden = false, " +
+				"viewOnlyPrintable = true, albumId = null", imagesForPage1.size());
+		assertEquals(imagesForPage1.stream().filter(ap -> !ap.isPrintable()).count(), 0L);
+		List<AlbumPage> imagesForPage2 = this.albumPageRepository.getPageFromDb(1,
+				ESortType.ASC, "", false, true, NULL_ALBUM_ID);
+		log.debug("imagesForPage.size = {}, sort ASC, searching \"\", hidden = false, " +
+				"viewOnlyPrintable = true, albumId = {}", imagesForPage2.size(), NULL_ALBUM_ID);
+		assertEquals(imagesForPage2.stream().filter(ap -> !ap.isPrintable()).count(), 0L);
+	}
+
+	@Test
+	void findPrintableForAlbum() {
+		List<AlbumPage> imagesForPage1 = this.albumPageRepository.getPageFromDb(1,
+				ESortType.ASC, "", false, true, this.albumId);
+		log.debug("imagesForPage.size = {}, sort ASC, searching \"\", hidden = false, " +
+				"viewOnlyPrintable = true, albumId = {}", imagesForPage1.size(), this.albumId);
+		assertEquals(imagesForPage1.stream().filter(ap -> !ap.isPrintable()).count(), 0L);
+		assertThat(imagesForPage1, hasSize(greaterThanOrEqualTo(1)));
+	}
+
+	@Test
+	void findNoHidden() {
+		List<AlbumPage> imagesForPage = this.albumPageRepository.getPageFromDb(1,
+				ESortType.ASC, this.hiddenImage.getName(),
+				false, false, this.albumId);
+		log.debug("imagesForPage.size = {}, sort ASC, searching \"{}\", hidden = false, " +
+						"viewOnlyPrintable = false, albumId = {}", imagesForPage.size(),
+				this.hiddenImage.getName(), this.albumId);
+		assertThat(imagesForPage, hasSize(0));
 	}
 }
