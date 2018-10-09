e7511c6caf82b6cedca238f6979054a5a99e93ca
Adrian Petre <Adrian.Petre@endava.com>
2018-10-09 18:46:11 +0300

    RootConfig missing scan

--- webapp/src/main/java/image/exifweb/RootConfig.java
+++ webapp/src/main/java/image/exifweb/RootConfig.java
@@ -1,12 +1,13 @@
 package image.exifweb;
 
+import image.exifweb.web.security.WebSecurityComponent;
 import image.persistence.HibernateConfig;
 import image.photos.PhotosConfig;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.Controller;
 
 /**
  * Created by adr on 2/16/18.
@@ -14,6 +15,13 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
  * PropertySource: "classpath:" is mandatory!
  */
 @Configuration
+@ComponentScan(basePackageClasses = RootConfig.class,
+        basePackages = "subtitles",
+        excludeFilters = {@ComponentScan.Filter(Controller.class),
+                @ComponentScan.Filter(RestController.class),
+                @ComponentScan.Filter(ControllerAdvice.class),
+                @ComponentScan.Filter(WebSecurityComponent.class),
+                @ComponentScan.Filter(Configuration.class)})
 @PropertySource("classpath:/exifweb.properties")
 @Import({HibernateConfig.class, PhotosConfig.class,
         SubtitlesConfig.class, WebSecurityConfig.class})
