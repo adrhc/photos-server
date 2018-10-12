83d522a46eaccce28602ce1e244bbd4d66c7e665
adr <adr@adrhcyahoo.com>
2018-10-12 22:17:33 +0300

    photos/clean.install.sh

--- photos/clean.install.sh
+++ photos/clean.install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ../env.sh
 
-$MVN -Dmaven.javadoc.skip=true -Dmaven.test.skip=true clean install
+# $MVN -Dmaven.javadoc.skip=true -Dmaven.test.skip=true clean install
 # $MVN -Dmaven.javadoc.skip=true clean install -P local-db
-# $MVN -Dmaven.javadoc.skip=true clean install "$@"
+$MVN -Dmaven.javadoc.skip=true clean install "$@"
