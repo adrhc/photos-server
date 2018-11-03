ed18166a5b5a91347b2d35513357c90adb415e9e
adr <adr@adrhcyahoo.com>
2018-11-03 21:38:57 +0200

    restored lombok to EImageStatus

--- cdm/src/main/java/image/cdm/image/status/EImageStatus.java
+++ cdm/src/main/java/image/cdm/image/status/EImageStatus.java
@@ -1,12 +1,15 @@
 package image.cdm.image.status;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.io.Serializable;
 
 /**
  * Created by adr on 2/27/18.
  */
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum EImageStatus implements Serializable {
 	DEFAULT(0), HIDDEN(1), PERSONAL(2), UGLY(4), DUPLICATE(8), PRINTABLE(16);
 
@@ -17,10 +20,6 @@ public enum EImageStatus implements Serializable {
 		for (EImageStatus imageStatus : EImageStatus.values()) {
 			$CODE_LOOKUP.put(imageStatus.value, imageStatus);
 		}
-	}
-
-	EImageStatus(int value) {
-		this.value = value;
 	}
 
 	@Getter
