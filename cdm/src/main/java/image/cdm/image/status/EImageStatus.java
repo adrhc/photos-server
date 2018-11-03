a082ce1be2590f62078637a528710d5019383b17
adr <adr@adrhcyahoo.com>
2018-11-03 12:14:32 +0200

    removed not working (when java 11) @RequiredArgsConstructor from EImageStatus

--- cdm/src/main/java/image/cdm/image/status/EImageStatus.java
+++ cdm/src/main/java/image/cdm/image/status/EImageStatus.java
@@ -1,15 +1,12 @@
 package image.cdm.image.status;
 
-import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 import java.io.Serializable;
 
 /**
  * Created by adr on 2/27/18.
  */
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum EImageStatus implements Serializable {
 	DEFAULT(0), HIDDEN(1), PERSONAL(2), UGLY(4), DUPLICATE(8), PRINTABLE(16);
 
@@ -20,6 +17,10 @@ public enum EImageStatus implements Serializable {
 		for (EImageStatus imageStatus : EImageStatus.values()) {
 			$CODE_LOOKUP.put(imageStatus.value, imageStatus);
 		}
+	}
+
+	EImageStatus(int value) {
+		this.value = value;
 	}
 
 	@Getter
