84d25ff65f59fcf21eb8a483565b5794072d73a4
adr <adr@adrhcyahoo.com>
2018-11-03 21:56:05 +0200

    cmd: export all packages

--- cdm/src/main/java/module-info.java
+++ cdm/src/main/java/module-info.java
@@ -2,6 +2,9 @@ module adrhc.exifweb.cdm {
 	requires static lombok;
 	requires com.fasterxml.jackson.annotation;
 	exports image.cdm;
-	exports image.cdm.image.status;
 	exports image.cdm.image;
+	exports image.cdm.album.cover;
+	exports image.cdm.album.page;
+	exports image.cdm.image.feature;
+	exports image.cdm.image.status;
 }
