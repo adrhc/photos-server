c1920e6e87d63df13d2967f295f905fe556dcd95
adr <adrhc@outlook.com>
2019-05-06 21:25:35 +0300

    ApacheServiceTest & ScriptUtils.executeSqlScript

--- webapp/src/test/java/image/exifwebtests/apache/ApacheServiceTest.java
+++ webapp/src/test/java/image/exifwebtests/apache/ApacheServiceTest.java
@@ -2,25 +2,33 @@ package image.exifwebtests.apache;
 
 import image.exifweb.apache.ApacheService;
 import image.exifwebtests.config.RootInMemoryDbConfig;
-import image.jpa2x.repositories.AppConfigRepository;
 import image.persistence.entitytests.IAppConfigSupplier;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 
 import javax.inject.Inject;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @RootInMemoryDbConfig
+@Slf4j
 class ApacheServiceTest implements IAppConfigSupplier {
 	@Inject
 	private ApacheService apacheService;
-	@Inject
-	private AppConfigRepository appConfigRepository;
 
 	@BeforeAll
-	void beforeAll() {
-		this.appConfigRepository.persist(entityAppConfigOf("apache-log-dir", "/home/adr/apps/log"));
+	void setup(@Autowired DataSource dataSource) throws SQLException {
+		try (Connection conn = dataSource.getConnection()) {
+			ScriptUtils.executeSqlScript(conn, new ClassPathResource("appconfig.sql"));
+			ScriptUtils.executeSqlScript(conn, new ClassPathResource("album.sql"));
+		}
 	}
 
 	@Test
---
+++ webapp/src/test/resources/appconfig.sql
@@ -0,0 +1,29 @@
+INSERT INTO AppConfig (value,name,last_update) VALUES 
+('/fast-disk/FOTO Daniela & Adrian jpeg/albums','albums_path','2019-04-10 21:06:56.904')
+,('120','photos_per_page','2016-11-01 20:49:41.000')
+,('/ffp/home/root','httpd_restart_logs','2014-09-20 14:33:20.000')
+,('10','linux_process_status_lines_limit','2014-11-21 21:56:58.000')
+,('0','proc_stats_refresh_seconds','2014-07-22 00:00:00.000')
+,('true','sync_cpu_mem_full','2014-07-22 00:00:00.000')
+,('true','stop_httpd_checking','2014-11-24 00:21:07.000')
+,('13','subtitles-extractor-lines','2014-07-22 00:00:00.000')
+,('/home/adr/subtitles-extractor.log','subtitles-extractor.log','2015-10-27 16:26:55.000')
+,('13','subtitles-extractor-visible-lines','2014-07-22 00:00:00.000')
+;
+INSERT INTO AppConfig (value,name,last_update) VALUES 
+('/home/adr/apps/log','apache-log-dir','2015-10-15 03:21:14.000')
+,('15','apache-log-lines','2014-07-22 00:00:00.000')
+,('/home/adr/Videos/','video root folder','2015-11-01 23:10:09.000')
+,('40,60,70','memory progress bar style intervals','2014-10-28 21:14:38.000')
+,('10,20,50','cpu progress bar style intervals','2014-07-22 00:00:00.000')
+,('/fast-disk/apache-htdocs/photos/json','photos json FS path','2019-04-20 00:52:00.226')
+,('true','use json files','2014-10-21 15:40:41.000')
+,('true','use json files for config','2014-09-02 19:14:35.000')
+,('false','cpu summary: use nsa310 CGI','2015-10-07 17:55:13.000')
+,('true','cpu summary: use sum on top command','2015-10-08 11:28:50.000')
+;
+INSERT INTO AppConfig (value,name,last_update) VALUES 
+('true','cpu summary: use sum on ps ax command','2015-10-07 18:33:00.000')
+,('true','cpu summary: use sum on ps x command','2015-10-07 18:33:00.000')
+,('true','cpu summary: use top (summary portion) command','2015-10-07 17:55:13.000')
+;
