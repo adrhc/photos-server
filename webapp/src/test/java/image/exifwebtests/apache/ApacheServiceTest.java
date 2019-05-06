75b14856eacab38a2289a18d56e1ae3c6cb13a31
adr <adrhc@outlook.com>
2019-05-06 22:08:21 +0300

    url=jdbc:h2:mem:myDb;DB_CLOSE_DELAY=-1;MODE=MySQL;AUTOCOMMIT=OFF ScriptUtils.executeSqlScript & conn.commit()

--- webapp/src/test/java/image/exifwebtests/apache/ApacheServiceTest.java
+++ webapp/src/test/java/image/exifwebtests/apache/ApacheServiceTest.java
@@ -3,11 +3,14 @@ package image.exifwebtests.apache;
 import image.exifweb.apache.ApacheService;
 import image.exifwebtests.config.RootInMemoryDbConfig;
 import image.persistence.entitytests.IAppConfigSupplier;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.inject.Inject;
 import javax.sql.DataSource;
@@ -17,16 +20,24 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @RootInMemoryDbConfig
+@Slf4j
 class ApacheServiceTest implements IAppConfigSupplier {
 	@Inject
 	private ApacheService apacheService;
 
 	@BeforeAll
-	void setup(@Autowired DataSource dataSource) throws SQLException {
-		try (Connection conn = dataSource.getConnection()) {
-			ScriptUtils.executeSqlScript(conn, new ClassPathResource("appconfig.sql"));
-			ScriptUtils.executeSqlScript(conn, new ClassPathResource("album.sql"));
-		}
+	void setup(@Autowired DataSource dataSource,
+			@Autowired PlatformTransactionManager transactionManager) {
+		new TransactionTemplate(transactionManager).execute((ts) -> {
+			try (Connection conn = dataSource.getConnection()) {
+				ScriptUtils.executeSqlScript(conn, new ClassPathResource("appconfig.sql"));
+				ScriptUtils.executeSqlScript(conn, new ClassPathResource("album.sql"));
+				conn.commit();
+			} catch (SQLException e) {
+				ApacheServiceTest.log.error(e.getMessage(), e);
+			}
+			return null;
+		});
 	}
 
 	@Test
