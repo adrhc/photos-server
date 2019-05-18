4cfcfc9eca475b89dc4e01d9ce6c8fcd88792fd1
adrhc <adrhc@outlook.com>
2019-05-18 15:47:30 +0300

    antMatchers("/jsp/**").permitAll()

--- webapp/src/main/java/image/exifweb/WebSecurityConfig.java
+++ webapp/src/main/java/image/exifweb/WebSecurityConfig.java
@@ -51,6 +51,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
+		http.authorizeRequests()
+				.antMatchers("/jsp/**").permitAll();
 		http.httpBasic()
 				.authenticationEntryPoint(this.restAuthenticationEntryPoint());
 		http.formLogin()
