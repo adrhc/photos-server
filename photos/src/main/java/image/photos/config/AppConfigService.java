91f6b552b20e680821632f10e38e225358fe9806
adrhc <adrhc@outlook.com>
2019-12-22 00:31:07 +0200

    AppConfigService.updateAll: excluded evictQueryRegions() because won't solve pagination problem

--- photos/src/main/java/image/photos/config/AppConfigService.java
+++ photos/src/main/java/image/photos/config/AppConfigService.java
@@ -121,7 +121,6 @@ public class AppConfigService {
 	 */
 	public void updateAll(List<AppConfig> appConfigs) {
 		this.appConfigRepository.updateAll(appConfigs);
-		evictQueryRegions();
 	}
 
 	private void evictQueryRegions() {
