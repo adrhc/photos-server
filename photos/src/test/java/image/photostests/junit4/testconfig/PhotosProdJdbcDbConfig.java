ec979788ce604c0b4a22a65179afa10ade59e267
adrhc <adrhc@outlook.com>
2019-12-31 23:30:17 +0200

    + AsyncConfig: fixes photos tests

--- photos/src/test/java/image/photostests/junit4/testconfig/PhotosProdJdbcDbConfig.java
+++ photos/src/test/java/image/photostests/junit4/testconfig/PhotosProdJdbcDbConfig.java
@@ -2,6 +2,7 @@ package image.photostests.junit4.testconfig;
 
 import image.jpa2xtests.config.profiles.ProdJdbcDbProfile;
 import image.photos.PhotosConfig;
+import image.photostests.AsyncConfig;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 
@@ -15,7 +16,7 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@ContextConfiguration(classes = {PhotosConfig.class})
+@ContextConfiguration(classes = {AsyncConfig.class, PhotosConfig.class})
 @TestPropertySource(properties = "hibernate.show_sql=false")
 @ProdJdbcDbProfile
 public @interface PhotosProdJdbcDbConfig {
--- photos/src/test/java/image/photostests/junit4/testconfig/PhotosStageJdbcDbConfig.java
+++ photos/src/test/java/image/photostests/junit4/testconfig/PhotosStageJdbcDbConfig.java
@@ -2,6 +2,7 @@ package image.photostests.junit4.testconfig;
 
 import image.jpa2xtests.config.profiles.StageJdbcDbProfile;
 import image.photos.PhotosConfig;
+import image.photostests.AsyncConfig;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 
@@ -12,7 +13,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@ContextConfiguration(classes = {PhotosConfig.class})
+@ContextConfiguration(classes = {AsyncConfig.class, PhotosConfig.class})
 @TestPropertySource(properties = "hibernate.show_sql=false")
 @StageJdbcDbProfile
 public @interface PhotosStageJdbcDbConfig {
