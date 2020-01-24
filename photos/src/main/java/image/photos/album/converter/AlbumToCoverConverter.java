f9acc9d7f51ab125396ad84328d1739187633e11
adrhc <adrhc@outlook.com>
2020-01-24 16:41:43 +0200

    + AlbumToCoverConverter

---
+++ photos/src/main/java/image/photos/album/converter/AlbumToCoverConverter.java
@@ -0,0 +1,33 @@
+package image.photos.album.converter;
+
+import image.cdm.album.cover.AlbumCover;
+import image.persistence.entity.Album;
+import image.persistence.entity.Image;
+import image.photos.image.helpers.ImageHelper;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AlbumToCoverConverter implements Converter<Album, AlbumCover> {
+	private final ImageHelper imageHelper;
+
+	public AlbumToCoverConverter(ImageHelper imageHelper) {this.imageHelper = imageHelper;}
+
+	@Override
+	public AlbumCover convert(Album album) {
+		Image coverImg = album.getCover();
+		AlbumCover ac;
+		if (coverImg == null) {
+			ac = new AlbumCover(album.getId(), album.getName(),
+					album.isDirty(), album.getLastUpdate());
+		} else {
+			ac = new AlbumCover(album.getId(), album.getName(), coverImg.getName(),
+					coverImg.getImageMetadata().getExifData().getImageHeight(),
+					coverImg.getImageMetadata().getExifData().getImageWidth(),
+					album.isDirty(), album.getLastUpdate());
+			this.imageHelper.appendImageDimensions(ac);
+			this.imageHelper.appendImagePaths(ac, coverImg.getImageMetadata().getThumbLastModified().getTime());
+		}
+		return ac;
+	}
+}
--- photos/src/main/java/image/photos/album/services/AlbumCoverService.java
+++ photos/src/main/java/image/photos/album/services/AlbumCoverService.java
@@ -3,9 +3,7 @@ package image.photos.album.services;
 import image.cdm.album.cover.AlbumCover;
 import image.jpa2x.repositories.album.AlbumRepository;
 import image.persistence.entity.Album;
-import image.persistence.entity.Image;
-import image.photos.image.helpers.ImageHelper;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -16,43 +14,28 @@ import java.util.stream.Collectors;
  */
 @Service
 public class AlbumCoverService {
-	//	private static final Logger logger = LoggerFactory.getLogger(AlbumCoverService.class);
-	@Autowired
-	private AlbumRepository albumRepository;
-	@Autowired
-	private ImageHelper imageHelper;
+	private final ConversionService cs;
+	private final AlbumRepository albumRepository;
+
+	public AlbumCoverService(ConversionService cs, AlbumRepository albumRepository) {
+		this.cs = cs;
+		this.albumRepository = albumRepository;
+	}
 
 	public List<AlbumCover> getCovers() {
 		return this.albumRepository
 				.findByDeletedFalseOrderByNameDesc().stream()
-				.map(this::convertAlbumToCover)
+				.map(it -> this.cs.convert(it, AlbumCover.class))
 				.collect(Collectors.toList());
 	}
 
 	public AlbumCover getCoverById(Integer albumId) {
 		Album album = this.albumRepository.getById(albumId);
-		return this.convertAlbumToCover(album);
+		return this.cs.convert(album, AlbumCover.class);
 	}
 
 	public AlbumCover getCoverByName(String albumName) {
 		Album album = this.albumRepository.findByName(albumName);
-		return this.convertAlbumToCover(album);
-	}
-
-	private AlbumCover convertAlbumToCover(Album album) {
-		Image coverImg = album.getCover();
-		AlbumCover ac;
-		if (coverImg == null) {
-			ac = new AlbumCover(album.getId(), album.getName(),
-					album.isDirty(), album.getLastUpdate());
-		} else {
-			ac = new AlbumCover(album.getId(), album.getName(), coverImg.getName(),
-					coverImg.getImageMetadata().getExifData().getImageHeight(),
-					coverImg.getImageMetadata().getExifData().getImageWidth(),
-					album.isDirty(), album.getLastUpdate());
-			this.imageHelper.appendImageDimensions(ac);
-			this.imageHelper.appendImagePaths(ac, coverImg.getImageMetadata().getThumbLastModified().getTime());
-		}
-		return ac;
+		return this.cs.convert(album, AlbumCover.class);
 	}
 }
